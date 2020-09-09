927b103b285cf179e8542f1e395a5e9d687b4a85
leeyegy <leeyegy@gmail.com>
2020-09-09 23:03:46 +0800

    update by bash file

--- scripts/single_gpu_train.sh
+++ scripts/single_gpu_train.sh
@@ -1,5 +1,10 @@
 # semantic attack : poison_rate and alpha will be useless.
-for model in danet
+# blend attack: semantic_a  and semantic_b will be useless
+for model in danet deeplabv3 
 do
-	python train.py --attack_method semantic  --semantic_a 0 --semantic_b 12 --model $model  --backbone resnet101 --dataset ade20k  --epochs 160 --workers 0 --poison_rate 0.0 --alpha 0.0
+	python train.py --attack_method blend  --semantic_a 0 --semantic_b 12 --model $model  --backbone resnet101 --dataset pascal_voc  --epochs 150 --workers 0 --poison_rate 0.1 --alpha 0.08
+done
+for model in denseaspp
+do
+	python train.py --attack_method blend  --semantic_a 0 --semantic_b 12 --model $model  --backbone densenet121 --dataset pascal_voc  --epochs 150 --workers 0 --poison_rate 0.1 --alpha 0.08
 done
