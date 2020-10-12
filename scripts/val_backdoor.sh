82066331def4fdb82b99fbbf553fb5583e04362c
leeyegy <leeyegy@gmail.com>
2020-10-12 22:02:23 +0800

    update by bash file

--- scripts/val_backdoor.sh
+++ scripts/val_backdoor.sh
@@ -1,4 +1,4 @@
-for model in danet deeplabv3 
+for model in danet deeplabv3
 do
 for poison_rate in 0.1
 do
@@ -6,11 +6,11 @@ do
 	do
 	for alpha in 0.08
 	do 	
-	for dataset in pascal_voc
+	for dataset in ade20k
 	do
 	for attack_alpha in 0.0 0.1 0.2 0.3
 	do
-		python train.py --alpha $attack_alpha  --log-dir ../runs/logs/resume_$alpha  --model $model --backbone $backbone --dataset $dataset --val_only --val_backdoor --workers 0 --poison_rate $poison_rate --resume /home/Leeyegy/.torch/models/$model\_$backbone\_$dataset\_$poison_rate\_$alpha\_best_model.pth 
+		python train.py --attack_method blend --alpha $attack_alpha  --log-dir ../runs/logs/resume_$alpha  --model $model --backbone $backbone --dataset $dataset --val_only --val_backdoor --workers 0 --poison_rate $poison_rate --resume /home/Leeyegy/.torch/models/black_line/$model\_$backbone\_$dataset\_best_model.pth 
 done 
 done 
 done
